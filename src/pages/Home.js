5e33b1218afc7004d518c49f232f00e3ab635169
leticea <nascimento.leticeah@gmail.com>
2023-03-11 15:42:49 -0300

    criando o footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,16 +2,12 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 import BannerImage from "../assets/pizza.jpeg";
-
 import "../styles/Home.css";
 
 export function Home() {
   return (
-    <div className="home">
-      <div
-        className="headerContainer"
-        style={{ backgroundImage: `url(${BannerImage})` }}
-      >
+    <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
+      <div className="headerContainer">
         <h1>Letícia's Pizzeria</h1>
         <p> PIZZA TO FIT ANY TASTE</p>
         <NavLink to="/menu">
