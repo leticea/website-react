8cf402b3ec378d46e5207cb704b03b4d5d92cb28
leticea <nascimento.leticeah@gmail.com>
2023-03-11 11:24:16 -0300

    criando as rotas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
 import { Navbar } from "./components/Navbar";
+import "./App.css";
 
 export function App() {
   return (
     <div className="App">
       <Navbar />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </div>
   );
 }
