5209d7c03507b612e627a33b4d3bfabb1aceb307
leticea <nascimento.leticeah@gmail.com>
2023-03-11 15:54:07 -0300

    criando o menu

---
+++ src/pages/Menu.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Menu() {
+  return (
+    <div className="menu">
+      <h1 className="menuTitle">Our Menu</h1>
+      <div className="menuList">
+      </div>
+    </div>
+  )
+}
