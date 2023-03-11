c342aa320ae46894dee4705b6a728752da701512
leticea <nascimento.leticeah@gmail.com>
2023-03-11 15:07:51 -0300

    inserindo a imagem da home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,23 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+
+import BannerImage from "../assets/pizza.jpeg";
+
+import "../styles/Home.css";
 
 export function Home() {
-  return <div>Hello world</div>;
+  return (
+    <div className="home">
+      <div
+        className="headerContainer"
+        style={{ backgroundImage: `url(${BannerImage})` }}
+      >
+        <h1>Letícia's Pizzeria</h1>
+        <p> PIZZA TO FIT ANY TASTE</p>
+        <NavLink to="/menu">
+          <button> ORDER NOW </button>
+        </NavLink>
+      </div>
+    </div>
+  );
 }
